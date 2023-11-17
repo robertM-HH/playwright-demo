df2cc8bf52c840b950bca0b99ac9bbd7406e63fa
Robert <robert.milcu@eventim.de>
2023-11-17 21:01:07 +0100

    commented out beforeEach and afterEach

--- tests/testWebpage.spec.ts
+++ tests/testWebpage.spec.ts
@@ -1,24 +1,25 @@
 import { test, expect} from '@playwright/test';
 import { LoginPage } from '../src/pages/LoginPage';
 
-test.beforeEach('Open start URL', async ({ page }, testInfo) => {
-    console.log(`Running ${testInfo.title}`);
-    const loginPage = new LoginPage(page);
-    await loginPage.loginToWebsite();
-    });
+// test.beforeEach('Open start URL', async ({ page }, testInfo) => {
+//     console.log(`Running ${testInfo.title}`);
+//     const loginPage = new LoginPage(page);
+//     await loginPage.loginToWebsite();
+//     });
 
 
-test.afterEach(async ({ page }, testInfo) => {
-    console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+// test.afterEach(async ({ page }, testInfo) => {
+//     console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
     
-    if (testInfo.status !== testInfo.expectedStatus)
-        console.log(`Did not run as expected, ended up at ${page.url()}`);
-    });
+//     if (testInfo.status !== testInfo.expectedStatus)
+//         console.log(`Did not run as expected, ended up at ${page.url()}`);
+//     });
  
       
 
 test('can login to website @fast',async ({page}) => {
-    const loginpage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
+    await loginPage.loginToWebsite();
     /**
      * To set a specific browser size:
      * const customViewportSize = { width: 900, height: 600 };
